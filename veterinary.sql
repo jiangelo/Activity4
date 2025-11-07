26837497a13d96d5d15d9977eacb5e94606d377c
Jian Angelo N. Mendoza <24-38830@g.batstate-u.edu.ph>
2025-11-07 22:14:10 +0800

    feat/create-table-appointments

--- veterinary.sql
+++ veterinary.sql
@@ -7,3 +7,11 @@ CREATE TABLE owners(
     phone VARCHAR(15),
     email VARCHAR(100)
 );
+
+CREATE TABLE appointments (
+    appointid INT PRIMARY KEY,
+    animalid INT,
+    appointdate DATE,
+    reason VARCHAR(255),
+    FOREIGN KEY (animalid) REFERENCES animals(animalid)
+);
