a5cc6276ec5fde5bd4ad367339ad3a96807f1519
Riza M. Torres <24-34454@g.batstate-u.edu.ph>
2025-11-07 22:33:54 +0800

    feat/create-table-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
---Veterinary DATABASE features will be added here
-CREATE TABLE owners(
-    ownerid INT PRIMARY KEY,
-    ofirstname VARCHAR(50),
-    olastname VARCHAR(50),
-    address VARCHAR(100),
-    phone VARCHAR(15),
-    email VARCHAR(100)
-);
-
-CREATE TABLE appointments (
-    appointid INT PRIMARY KEY,
-    animalid INT,
-    appointdate DATE,
-    reason VARCHAR(255),
-    FOREIGN KEY (animalid) REFERENCES animals(animalid)
-=======
 CREATE DATABASE veterinary;
 
 CREATE TABLE animals(
@@ -31,10 +13,10 @@ FOREIGN KEY (ownerid)
 REFERENCES owners(ownerid)
 );
 
-CREATE TABLE appointments (
-    appointid INT PRIMARY KEY,
-    animalid INT,
-    appointdate DATE,
-    reason VARCHAR(255),
-    FOREIGN KEY (animalid) REFERENCES animals(animalid)
+CREATE TABLE doctors(
+dfirstname VARCHAR(50),
+dlastname VARCHAR(50),
+speciality VARCHAR(100),
+phone VARCHAR (15),
+email VARCHAR(100)
 );
